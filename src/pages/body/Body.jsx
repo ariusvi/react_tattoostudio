7a63210bdd677e8852245524b50f3c7cc39a86ed
Ana Rius <a.riusvi11@gmail.com>
2024-03-21 19:54:34 +0100

    feat: body.jsx base structure created

---
+++ src/pages/body/Body.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+
+export const Body = () => {
+    return (
+        <Routes>
+            {/* aqui iran las vistas*/}
+        </Routes>
+    )
+}
