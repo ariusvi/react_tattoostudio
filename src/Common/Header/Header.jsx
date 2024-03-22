403eaaf6abd913e2d75a88c7360a48c5ccc69f0f
Ana Rius <a.riusvi11@gmail.com>
2024-03-22 19:22:15 +0100

    feat: custom link header edited not finished

--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -1,11 +1,28 @@
 import './Header.css'
 
-export const Header = () => {
+import { CustomLink } from '../CustomLink/CustomLink'
 
+export const Header = () => {
+    const token = true;
     return (
 
         <div className='headerDesign'>
-            Soy el Header
+            <CustomLink title="Home" destination="/" />
+            {
+                token ? (
+                    <div>
+                        <CustomLink title="name"    destination="/profile" />
+                        <CustomLink title="Log-out" destination="/" />
+                    </div>
+                ) 
+                : (
+                    <div>
+                        <CustomLink title="Login"   destination="/login" />
+                        <CustomLink title="Register"   destination="/register" />
+                </div>)
+            }
+            <CustomLink title="Services" destination="/facilities" />
+            <CustomLink title="Login" destination="/login" />
         </div>
 
     )
