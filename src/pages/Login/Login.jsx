4c3107250d47b8226fb3cfb0e86e3398bbb08d27
Ana Rius <a.riusvi11@gmail.com>
2024-03-23 17:11:26 +0100

    feat: update login logic throwing error message

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
+import './Login.css'
 
 import { useState, useEffect } from 'react';
 import { CustomInput } from '../../Common/CustomInput/CustomInput';
 import { loginMe } from '../../services/apiCalls';
-import './Login.css'
+import { decodeToken } from "react-jwt";
+
 
 export const Login = () => {
 
@@ -11,13 +13,15 @@ export const Login = () => {
         password: "",
     });
 
+    const [msgError, setMsgError] = useState("") //lanzar el mensaje de error
+
     const inputHandler = (e) => {
         // In the CustomInput component, there is a function called functionChange with the inputHandler object. 
         // This function triggers an onChange event in the setCredenciales field.
-        
+
         setCredenciales((prevState) => ({
             ...prevState, // Shallow copy of the previous state to avoid direct modification. (prevState is a placeholder)
-                          // (...) creates a new instance.
+            // (...) creates a new instance.
             [e.target.name]: e.target.value // e.target accesses the name property of the object to be modified. [] because it access to the property refeared in e.target.name
             //e.target.value access to the field "name" to modified the data.
             // if i write in email, e.target.name is .. email.
@@ -26,23 +30,42 @@ export const Login = () => {
     };
 
     const logMe = async () => {
-        const fetched = await loginMe(credenciales)
 
-        console.log(fetched)
+        
+
+            const fetched = await loginMe(credenciales)
+
+            if (!fetched.success) {
+                setMsgError(fetched.message)
+                return;
+            }
+
+            // if (!fetched.token) {
+            //     return              //todo terminar esto
+            // }
+
+            const decodificado = decodeToken(fetched.token) //decodificamos el token
+
+            sessionStorage.setItem("token", fetched)
+            sessionStorage.setItem("user", JSON.stringify(decodificado))
+
+            //redirigir a Home
+        
+
     }
 
-    return(
+    return (
         <div className='loginDesign'>
             {/* <pre>{JSON.stringify(credenciales, null, 2)}</pre> */}
-            <CustomInput 
-                design="inputDesign" 
+            <CustomInput
+                design="inputDesign"
                 type="email"
                 name="email"
                 value={credenciales.email || ""}
                 placeholder="write your email"
                 functionChange={inputHandler}
             />
-            <CustomInput 
+            <CustomInput
                 design="inputDesign"
                 // design={credenciales.errores ? "inputDesign inputerror" : "inputDesign"} //todo
                 type="password"
@@ -52,6 +75,8 @@ export const Login = () => {
                 functionChange={inputHandler}
             />
             <div className="loginButton" onClick={logMe}>Log in</div>
+            <div>{msgError}</div> 
+            {/* cuando hay un error al loggearse, muestra el mensaje de error definido en backend */}
         </div>
     )
 };
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -10,9 +10,19 @@ export const loginMe = async (credenciales) => {
         body: JSON.stringify(credenciales),
     };
 
+    try {
+        
     // auth/login
     const response = await fetch(`${root}auth/login`, options)
     const data = await response.json()
+        
+    if (!data.success) {
+        throw new Error (data.message)
+    }
 
     return data;
-}
+    } catch (error) {
+        return error
+    }
+
+};
