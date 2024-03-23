c3c0f98ceb25a06868dc40dea389c37977a3543f
Ana Rius <a.riusvi11@gmail.com>
2024-03-23 10:58:05 +0100

    feat: login inputHandler created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,23 @@
 import './Login.css'
+import { useState, useEffect } from 'react';
 
 export const Login = () => {
 
-    return (
-        <div className='loginDesign'>
-            Soy la vista Login
-        </div>
-    )
-}
+    const [credenciales, setCredenciales] = useState({ //el array con las credenciales actuales del usuario // la funcion que actualiza ese estado
+        email: "",
+        password: ""
+    })
+
+    const inputHandler = (e) => {
+        // In the CustomInput component, there is a function called functionChange with the inputHandler object. 
+        // This function triggers an onChange event in the setCredenciales field.
+        
+        setCredenciales((prevState) => ({
+            ...prevState, // Shallow copy of the previous state to avoid direct modification. (prevState is a placeholder)
+                          // (...) creates a new instance.
+            [e.target.name]: e.target.value // e.target accesses the name property of the object to be modified. [] because it access to the property refeared in e.target.name
+            //e.target.value access to the field "name" to modified the data.
+            // if i write in email, e.target.name is .. email.
+            // if i write in password, e.target.name is .. password.
+        }));
+    }};
