8236001b4ef3476b8f564c02443674bee3193ec2
Ana Rius <a.riusvi11@gmail.com>
2024-03-26 22:48:19 +0100

    feat: add appointment data fetching logic

--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -2,9 +2,37 @@ import './Appointments.css'
 
 import { Header } from '../../Common/Header/Header';
 import { useState, useEffect } from "react";
+import { getAppointments } from '../../services/apiCalls';
 
 export const Appointments = () => {
     const datosUser = JSON.parse(localStorage.getItem("passport"));
+    const [tokenStorage, setTokenStorage] = useState(datosUser?.token)
+    const [loadedData, setLoadedData] = useState(false)
+    const [appointments, setAppointments] = useState([])
+
+    useEffect(() => {
+
+
+        const getDataAppointment = async () => {
+            try {
+
+                const fetched = await getAppointments(tokenStorage)
+                setAppointments(fetched.data.appointment)
+                setLoadedData(true)
+                console.log(fetched.data, "patata") //devuelve array de los appointments del usuario logeado
+                //datos del array: fecha de la cita (dateAppointment) y la id de la cita (id)
+                console.log(appointments, "fresas") //array vacio
+
+                //todo ¿deberia revisar para que devuelva tambien el nombre de la cita?
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        if (!loadedData) {
+            getDataAppointment()
+        }
+    }, [appointments])
 
     return (
         <>
