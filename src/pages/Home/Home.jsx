d85cc63e9253c50f566537e33f38319b90cf209a
Ana Rius <a.riusvi11@gmail.com>
2024-04-01 14:11:50 +0200

    feat: update Home page design and content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,36 @@
 import { Header } from '../../Common/Header/Header'
 import './Home.css'
+import homePhoto from '../../img/home_photo.jpg';
+import { CustomButton } from '../../Common/CustomButton/CustomButton';
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
 
+    const navigate = useNavigate();
+
     return (
         <>
-        <Header />
-        <div className='homeDesign'>
-            Soy la vista Home
-        </div>
+            <Header />
+            <div className='title'>Old Ink</div>
+            <div className='homeDesign'>
+                <div><img src={homePhoto} alt="tattoo_oldink" /></div>
+                <div className='text'>¡Bienvenido a Old Ink! <br></br>
+                    Somos un equipo apasionado de artistas del tatuaje dedicados a convertir tus ideas en obras de arte permanentes. Con un enfoque en la calidad, la creatividad y la seguridad, estamos aquí para ayudarte a expresar tu individualidad a través de impresionantes tatuajes personalizados. Explora nuestra galería y déjanos ser parte de tu próxima aventura en el mundo del arte corporal. ¡Bienvenido a nuestra comunidad de autoexpresión!
+                </div>
+                <div className='homeButtons'>
+                    <CustomButton
+                        className={'CustomButtonDesign'}
+                        title={'Servicios'}
+                        onClick={() => navigate("/facilities")}
+                    /></div>
+                <div className='homeButtons'>
+                    <CustomButton
+                        className={'CustomButtonDesign'}
+                        title={'Registrate'}
+                        onClick={() => navigate("/register")}
+                    /></div>
+            </div>
         </>
-        
+
     )
 }
